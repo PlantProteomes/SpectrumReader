4ab65a5b026f25387cae12b97f37bbd7105b4d38
Iceylettuce <nathan.hansen.zhang@gmail.com>
2022-09-27 14:46:35 -0700

    .\findspectra.py

--- scripts/findspectra.py
+++ scripts/findspectra.py
@@ -20,8 +20,9 @@ def main():
     argparser = argparse.ArgumentParser(description='An example program that reads an mzML file sequentially')
     argparser.add_argument('--mzml_file', action='store', help='Name of the mzML file to read')
     argparser.add_argument('--precursor_mz', action='store', type=float, help='Precursor m/z to select')
-    argparser.add_argument('--tolerance', action='store', type=float, help='Tolerance of a specified peak m/z and a measured peak m/z')
+    argparser.add_argument('--tolerance_mz', action='store', type=float, help='Tolerance of a m/z and the measured peak m/z')
     argparser.add_argument('--required_peaks', action='store', help='Tolerance of a specified peak m/z and a measured peak m/z')
+    argparser.add_argument('--tolerance_peaks', action='store', type=float, help='Tolerance of a m/z and the measured peak m/z')
     # runs the parse_args method to store all the results into the params variable
     params = argparser.parse_args()
 
@@ -40,16 +41,21 @@ def main():
         hasMZCheck = True
 
     # ensures there is always a tolerance
-    if params.tolerance is None or params.tolerance == 0.0:
-        params.tolerance = 0.01
-    print(f"tolerance is {params.tolerance}")
+    if params.tolerance_mz is None or params.tolerance_mz == 0.0:
+        params.tolerance_mz = 0.01
+    print(f"tolerance_mz is {params.tolerance_mz}")
+
+    if params.tolerance_peaks is None or params.tolerance_peaks == 0.0:
+        params.tolerance_peaks = 0.01
+    print(f"tolerance_peaks is {params.tolerance_peaks}")
 
     if (hasMZCheck):
-        lowerBound = params.precursor_mz - params.tolerance
-        upperBound = params.precursor_mz + params.tolerance
+        lowerBound = params.precursor_mz - params.tolerance_mz
+        upperBound = params.precursor_mz + params.tolerance_mz
 
     needToCheckPeaks = False
-    if not (params.required_peaks is None or params.required_peaks == ""):
+    # if not (params.required_peaks is None or params.required_peaks == ""):
+    if params.required_peaks is not None and params.required_peaks != "":
         requiredPeaksList = [float(i) for i in params.required_peaks.split(',')]
         needToCheckPeaks = True
         print(f"required peaks are {requiredPeaksList}")
@@ -72,20 +78,26 @@ def main():
                 stats['ms1spectra'] += 1
             elif spectrum['ms level'] == 2:
                 stats['ms2spectra'] += 1
-                precursor_mz = (((((((spectrum['precursorList'])['precursor']))[0])['selectedIonList'])['selectedIon'])[0])['selected ion m/z']
-                if (hasMZCheck and ((precursor_mz <= lowerBound) or (precursor_mz >= upperBound))):
+                precursor_mz = spectrum['precursorList']['precursor'][0]['selectedIonList']['selectedIon'][0]['selected ion m/z']
+                if hasMZCheck and (precursor_mz < lowerBound or precursor_mz > upperBound):
                     satisfyRequirements = False
-                if (needToCheckPeaks):
+                if needToCheckPeaks and satisfyRequirements:
                     for peak in requiredPeaksList:
-                        if satisfyRequirements:
-                            satisfyRequirements = peak in spectrum['intensity array']
+                        if spectrum['m/z array'][len(spectrum['m/z array']) - 1] < lowerPeakBound:
+                            satisfyRequirements = False
                         else:
-                            break
-                    if satisfyRequirements:
-                        validSpectra.append(spectrum)
-                else:
-                    if satisfyRequirements:
-                        validSpectra.append(spectrum)
+                            lowerPeakBound = peak - params.tolerance_peak
+                            upperPeakBound = peak + params.tolerance_peak
+                            for mz in spectrum['m/z array']:
+                                if mz < lowerPeakBound:
+                                    continue
+                                if mz > upperPeakBound:
+                                    satisfyRequirements = False
+                                    break
+                                break
+                        
+                if satisfyRequirements:
+                    validSpectra.append(spectrum)
 
             if stats['counter']/1000 == int(stats['counter']/1000):
                 print(f"  {stats['counter']}")
@@ -97,6 +109,7 @@ def main():
     print(f"The number of ms1spectra is {stats['ms1spectra']}")
     print(f"The number of ms2spectra is {stats['ms2spectra']}")
     print(f"Number of valid spectra: {len(validSpectra)}")
+    # write out mz and intensity array
     # print(f"Valid Spectra: {validSpectra}")
     print(f"INFO: Elapsed time: {t1-t0}")
     print(f"INFO: Processed {stats['counter']/(t1-t0)} spectra per second")
