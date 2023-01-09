b90592ae4e0dfba4997a9d2ba4060f809aefaa0f
Iceylettuce <nathan.hansen.zhang@gmail.com>
2023-01-09 15:19:58 -0800

    retested correlations

--- scripts/correlate_peaks.py
+++ scripts/correlate_peaks.py
@@ -99,6 +99,10 @@ class MSRunSpectraFinder:
                     if spectrum['m/z array'][len(spectrum['m/z array']) - 1] < self.lower_bound:
                         continue
                     else:
+                        closest_mz_1 = 0
+                        closest_mz_2 = 0
+                        known_intensity = 0
+                        current_intensity = 0
                         for index in range(len(spectrum['m/z array'])):
                             mz = spectrum['m/z array'][index]
                             if mz < self.lower_bound:
@@ -107,33 +111,33 @@ class MSRunSpectraFinder:
                                 break
                             else:
                                 # need to break after the first one as well?
-                                current_intensity = spectrum['intensity array'][index]
+                                if pair[0] - closest_mz_1 > pair[0] - mz:
+                                    closest_mz_1 = mz
+                                    current_intensity = spectrum['intensity array'][index]
 
-                                lower_peak_bound = pair[2] - self.peak_tolerance
-                                upper_peak_bound = pair[2] + self.peak_tolerance
-                                if spectrum['m/z array'][len(spectrum['m/z array']) - 1] < lower_peak_bound:
-                                        continue
+                                    lower_peak_bound = pair[2] - self.peak_tolerance
+                                    upper_peak_bound = pair[2] + self.peak_tolerance
+                                    if spectrum['m/z array'][len(spectrum['m/z array']) - 1] < lower_peak_bound:
+                                            continue
+                                    else:
+                                        for index in range(len(spectrum['m/z array'])):
+                                            if pair[2] - closest_mz_2 > pair[2] - index:
+                                                closest_mz_2 = index
+                                                mz = spectrum['m/z array'][index]
+                                                if mz < lower_peak_bound:
+                                                    continue
+                                                elif mz > upper_peak_bound:
+                                                    break
+                                                else:
+                                                    known_intensity = spectrum['intensity array'][index]
+                                                    continue
+                                            else:
+                                                continue
                                 else:
-                                    found = False
-                                    printed = False
-                                    for index in range(len(spectrum['m/z array'])):
-                                        mz = spectrum['m/z array'][index]
-                                        if mz < lower_peak_bound:
-                                            continue
-                                        elif mz > upper_peak_bound:
-                                            break
-                                        else:
-                                            known_intensity = spectrum['intensity array'][index]
-                                            peak_unknown_intensity.append(current_intensity)
-                                            peak_known_intensity.append(known_intensity)
-                                            # here lies the problem - make a record of everything and find the one closest to pair[2]
-                                            # if there is more than 1, either pick the brightest peak or the peak closest to the desired m/z value
-                                            # after inspecting the tolerance window, decide which one is the closest and then record the intensity
-                                            if found == True and printed == False and self.lower_bound == lower_peak_bound:
-                                                self.debug.append(spectrum)
-                                                printed = True
-                                            found = True
-                                            continue
+                                    continue
+                        if current_intensity != 0 and known_intensity != 0:
+                            peak_unknown_intensity.append(current_intensity)
+                            peak_known_intensity.append(known_intensity)
 
                     if self.stats_counter/1000 == int(self.stats_counter/1000):
                         print(f"  {self.stats_counter}")
